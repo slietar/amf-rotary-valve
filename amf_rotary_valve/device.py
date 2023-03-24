4c516b04578de4f27dc6d9775f1a1836ae573f5b
Simon Liétar <simon@lietar.net>
2023-03-24 12:38:02 +0100

    Improve disconnection logic

--- amf_rotary_valve/device.py
+++ amf_rotary_valve/device.py
@@ -11,10 +11,10 @@ from serial.serialutil import SerialException
 
 Datatype = type[bool] | type[int]
 
-class AMFDeviceConnectionLostError(Exception):
+class AMFDeviceConnectionError(Exception):
   pass
 
-class AMFDeviceConnectionError(Exception):
+class AMFDeviceConnectionLostError(AMFDeviceConnectionError):
   pass
 
 
@@ -76,7 +76,7 @@ class AMFDevice:
       await read_task
     except asyncio.CancelledError:
       pass
-    except (SerialException, asyncio.TimeoutError) as e:
+    except SerialException as e:
       raise AMFDeviceConnectionLostError from e
 
     if self._closed_exc.done() and (exc := self._closed_exc.exception()):
@@ -95,6 +95,10 @@ class AMFDevice:
       self._read_task = None
 
   async def close(self):
+    """
+    Closes the device.
+    """
+
     if not self._closing:
       self._closing = True
       futures = self._query_futures + ([self._busy_future] if self._busy_future else list())
@@ -112,6 +116,16 @@ class AMFDevice:
       pass
 
   async def closed(self):
+    """
+    Waits for the device to close.
+
+    When cancelled, closes the device and waits for it to close.
+
+    Raises
+      asyncio.CancelledError
+      AMFDeviceConnectionLostError: If the connection was lost rather than closed explicitly.
+    """
+
     try:
       await asyncio.shield(self._main_task)
     except asyncio.CancelledError:
@@ -144,11 +158,14 @@ class AMFDevice:
         assert (serial := self._serial)
         await loop.run_in_executor(None, lambda: serial.write(f"/_{command}\r".encode("utf-8")))
         return self._parse(await asyncio.wait_for(future, timeout=2.0), dtype=dtype)
-      except (SerialException, asyncio.TimeoutError) as e:
-        self._closed_exc.set_exception(e)
-        raise AMFDeviceConnectionError from e
-    except:
-      self._query_futures.remove(future)
+      except:
+        self._query_futures.remove(future)
+        raise
+    except (SerialException, asyncio.TimeoutError) as e:
+      self._closed_exc.set_exception(e)
+      await self.closed()
+
+      # This statement won't is unreachable as self.closed() will raise an AMFDeviceConnectionLostError.
       raise
 
   def _parse(self, data: bytes, dtype: Optional[Datatype] = None):
@@ -190,22 +207,62 @@ class AMFDevice:
       self._busy_future = None
 
   async def get_unique_id(self):
+    """
+    Returns the unique id of this device.
+
+    Returns
+      The unique id of this device, such as `...`.
+    """
+
     return await self._query("?9000")
 
   async def get_valve(self):
+    """
+    Returns the current valve position.
+
+    Returns
+      The current valve position, starting at 1, or `None` if the device has not been initialized.
+    """
+
     res = await self._query("?6", dtype=int)
     return res if res != 0 else None
 
   async def get_valve_count(self):
+    """
+    Returns the number of valves available on the rotary valve.
+    """
+
     return await self._query("?801", dtype=int)
 
   async def home(self):
+    """
+    Initializes the rotary valve.
+
+    The rotary valve must be initialized every time it is powered on. If it has already been initialized when calling this function, it will be re-initialized. The initialization status can be obtained by calling `get_valve()`, which returns `None` if the device has not yet been initialized.
+    """
+
     await self._run("ZR")
 
   async def rotate(self, valve: int, /):
+    """
+    Rotates the rotary valve to a new valve position.
+
+    A 360º rotation occurs if `valve` is already the current position. To avoid this behavior, check the current position with `get_valve()`.
+
+    Parameters
+      valve: The valve position to rotate to, starting at 1.
+    """
+
     await self._run(f"b{valve}R")
 
   async def wait(self, delay: float, /):
+    """
+    Instructs the rotary valve to wait for a fixed time duration.
+
+    Parameters
+      delay: The delay, in seconds. The resolution is 1 ms.
+    """
+
     await self._run(f"M{round(delay * 1000)}R")
 
   async def __aenter__(self):
@@ -216,6 +273,25 @@ class AMFDevice:
 
   @staticmethod
   def list(*, all: bool = False):
+    """
+    Lists visible devices.
+
+    Parameters
+      all: Whether to include devices that do not have recognized vendor and product ids.
+
+    Yields
+      Instances of `AMFDeviceInfo`.
+    """
+
     for item in serial.tools.list_ports.comports():
-      if all or (item.vid, item.pid) == (0x03eb, 0x2404):
+      # if all or (item.vid, item.pid) == (0x03eb, 0x2404):
+      if all:
         yield AMFDeviceInfo(address=item.device)
+
+
+__all__ = [
+  'AMFDevice',
+  'AMFDeviceConnectionError',
+  'AMFDeviceConnectionLostError',
+  'AMFDeviceInfo'
+]
